e85fe7242c97250081f1f8b65a04072204cadd43
Raymond Chong <mathsgod@yahoo.com>
2019-01-04 10:28:16 +0800

    init

---
+++ src/Client.php
@@ -0,0 +1,26 @@
+<?
+namespace GQL;
+
+class Client
+{
+    public function __construct()
+    {
+
+    }
+
+    public function query($query)
+    {
+
+    }
+
+    public function subscription($query)
+    {
+
+    }
+
+    public function mutation($query)
+    {
+
+    }
+
+}
